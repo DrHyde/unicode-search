109ddae69fdd007bc28d4f36ae70da9d35132785
David Cantrell <david@cantrell.org.uk>
2025-02-22 22:58:08 +0000

    Work around problem where Forge tries, by default, to build for distributions it can't on Linux
    Thanks to @erickzhao for his comment on https://github.com/electron/forge/issues/3772

--- forge.config.js
+++ forge.config.js
@@ -1,29 +1,30 @@
+const fs = require('fs');
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
+let makers = [
+  {
+    name: '@electron-forge/maker-squirrel',
+    config: {},
+  },
+  {
+    name: '@electron-forge/maker-zip',
+    platforms: ['darwin'],
+  },
+];
+
+if(fs.existsSync('/etc/redhat-release')) {
+  makers.push( { name: '@electron-forge/maker-rpm', config: {}, } );
+} else if(fs.existsSync('/etc/debian_version')) {
+  makers.push( { name: '@electron-forge/maker-deb', config: {}, } );
+}
 
 module.exports = {
   packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
-  makers: [
-    {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-  ],
+  makers: makers,
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
